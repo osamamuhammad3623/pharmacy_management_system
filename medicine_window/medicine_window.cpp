33933824b11cfc04a7e82c88b0f30adaceb1a125
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-04-29 13:05:05 +0200

    connecting Medicine window with db, done & tested

--- medicine_window/medicine_window.cpp
+++ medicine_window/medicine_window.cpp
@@ -1,6 +1,6 @@
 #include "medicine_window.h"
 #include "ui_medicine_window.h"
-#include "navigation.h"
+#include "api.h"
 
 Medicine_Window::Medicine_Window(QWidget *parent) :
     QWidget(parent),
@@ -8,10 +8,10 @@ Medicine_Window::Medicine_Window(QWidget *parent) :
 {
     ui->setupUi(this);
     /* add supplier names to Supplier combo-box */
-//    vector<Supplier> suppliers; // = get_suppliers;
-//    for (int i=0; i< suppliers.size();i++){
-//        ui->md_suppliers->addItem(QString::fromStdString(suppliers[i].name));
-//    }
+    vector<Supplier> suppliers = get_suppliers();
+    for (int i=0; i< suppliers.size();i++){
+        ui->md_suppliers->addItem(QString::fromStdString(suppliers[i].name));
+    }
 }
 
 Medicine_Window::~Medicine_Window()
@@ -21,7 +21,15 @@ Medicine_Window::~Medicine_Window()
 
 void Medicine_Window::on_check_shortage_clicked()
 {
+    vector<Medicine> meds_in_need = check_shortage(ui->max_quantity->value()); /* check for shortage meds */
+    ui->shortage_table->setRowCount(meds_in_need.size());
 
+    auto model = ui->shortage_table->model();
+    for (int i=0; i< meds_in_need.size(); i++){
+        model->setData(model->index(i,0),QString::fromStdString(meds_in_need[i].id));
+        model->setData(model->index(i,1),QString::fromStdString(meds_in_need[i].name));
+        model->setData(model->index(i,2),meds_in_need[i].quantity);
+    }
 }
 
 
@@ -34,7 +42,7 @@ void Medicine_Window::on_back_clicked()
 void Medicine_Window::on_add_medicine_clicked()
 {
     Medicine md;
-    md.id = ui->md_id->value();
+    md.id = to_string(ui->md_id->value());
     md.name = ui->md_name->toPlainText().toStdString();
     md.quantity = ui->md_quantity->value();
     md.purchase_price = ui->md_buy_price->value();
@@ -42,7 +50,8 @@ void Medicine_Window::on_add_medicine_clicked()
     md.category = ui->md_category->currentText().toStdString();
     md.supplier_company = ui->md_suppliers->currentText().toStdString();
 
-    // insert
+    string operation_result = insert_medicine(md);
+    ui->msg->setText(QString::fromStdString(operation_result));
 
     /* clear fields after insertion */
     ui->md_id->setValue(0);

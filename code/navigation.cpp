91b66afa6cca96c9b7ab5074365c6556c406ced6
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-05-17 00:56:14 +0200

    change windows titltes

--- code/navigation.cpp
+++ code/navigation.cpp
@@ -19,30 +19,35 @@ void launch_new_window(Window_Type type, QWidget* current_window){
 
         case GUI_Admin_Panel_Window:{
             Admin_Panel *new_window = new Admin_Panel();
+            new_window->setWindowTitle("Admin Panel");
             new_window->show();
             break;
         }
 
         case GUI_Pharmacist_Window:{
             Pharmacist_window *new_window = new Pharmacist_window();
+            new_window->setWindowTitle("Pharmacist Window");
             new_window->show();
             break;
         }
 
         case GUI_Login_Window:{
             MainWindow *new_window = new MainWindow;
+            new_window->setWindowTitle("Login");
             new_window->show();
             break;
         }
 
         case GUI_Supplier_Window:{
             supplier_window *new_window = new supplier_window();
+            new_window->setWindowTitle("Manage Suppliers");
             new_window->show();
             break;
         }
 
         case GUI_Pharmacist_Profile_Window:{
             pharmacist_profile_window *new_window = new pharmacist_profile_window();
+            new_window->setWindowTitle("Pharmacist Profile");
             new_window->show();
             break;
         }
